e6576fa7f5659218869d37cee73b3ec3cfff2324
Manuel Fernandez <manferlo81@gmail.com>
2019-08-25 01:08:46 +0700

    error refactoring

--- src/errors.ts
+++ src/errors.ts
@@ -4,7 +4,7 @@ export function error(msg: string) {
 
 export function errorFmt<F extends (...params: any[]) => Error>(template: string): F;
 export function errorFmt<F>(template: string): any {
-  return (...params: any[]) => new Error(
+  return (...params: any[]) => error(
     template.replace(
       /\$(\d+)/g,
       (_, index) => params[index],
@@ -12,22 +12,26 @@ export function errorFmt<F>(template: string): any {
   );
 }
 
-export function errorInvalid(obj: any, what: string) {
-  return error(`"${obj}" is not a valid ${what}`);
-}
+const msgInvalid = '"$0" is not a valid $1';
 
-export function errorInvalidType(type: string, what: string) {
-  return errorInvalid(type, `${what} type`);
-}
+export const errorInvalid = errorFmt<(obj: any, what: string) => Error>(
+  msgInvalid,
+);
+
+export const errorInvalidType = errorFmt<(obj: any, what: string) => Error>(
+  `${msgInvalid} type`,
+);
 
 export const errorNotInScope = errorFmt<(id: string) => Error>(
   "\"$0\" can't be found in this scope",
 );
 
+const msgRequired = '"$0" is required in a "$1"';
+
 export const errorRequired = errorFmt<(key: string, type: string) => Error>(
-  '"$0" is required in a "$1" expression',
+  `${msgRequired} expression`,
 );
 
 export const errorRequired2 = errorFmt<(key: string, type: string) => Error>(
-  '"$0" is required in a "$1" statement',
+  `${msgRequired} statement`,
 );
