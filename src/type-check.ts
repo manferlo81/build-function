5fe8470e617c5799c940a4b132295c4214172e34
Manuel Fernandez <manferlo81@gmail.com>
2019-11-10 15:46:47 +0700

    editor config

--- src/type-check.ts
+++ src/type-check.ts
@@ -1,5 +1,5 @@
 export const isArray = Array.isArray;
 
-export function isObj(param: unknown): param is Record<keyof any, any> {
+export function isObj(param: unknown): param is (null | Record<keyof any, any>) {
   return typeof param === "object";
 }
