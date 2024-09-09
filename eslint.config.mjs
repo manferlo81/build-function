15f26c93804e33090c839374fe21644954447939
Manuel Fernández <manferlo81@gmail.com>
2024-09-09 08:10:29 +0700

    chore: Refactor ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 import globals from 'globals';
 import { config, configs as typescriptConfigs } from 'typescript-eslint';
-import stylistic from '@stylistic/eslint-plugin';
 
 function normalizeRuleEntry(ruleEntry) {
   if (Array.isArray(ruleEntry)) return ruleEntry;
@@ -11,8 +11,8 @@ function normalizeRuleEntry(ruleEntry) {
 
 function createRuleNameNormalizer(pluginName) {
   if (!pluginName) return (ruleName) => ruleName;
+  const pluginPrefix = `${pluginName}/`;
   return (ruleName) => {
-    const pluginPrefix = `${pluginName}/`;
     if (ruleName.startsWith(pluginPrefix)) return ruleName;
     return `${pluginPrefix}${ruleName}`;
   };
@@ -34,24 +34,23 @@ const eslintRules = normalizeRules(null, {
 });
 
 const stylisticRules = normalizeRules('@stylistic', {
-  semi: 'always',
   indent: 2,
-  quotes: 'single',
   'linebreak-style': 'unix',
-
-  'quote-props': 'as-needed',
-  'arrow-parens': 'always',
   'no-extra-parens': 'all',
   'no-extra-semi': 'error',
-  'brace-style': '1tbs',
-
-  'member-delimiter-style': {},
   'padded-blocks': 'off',
 });
 
 const typescriptRules = normalizeRules('@typescript-eslint', {
-  // FIXME: turn rule ON
   'no-explicit-any': 'off',
+});
+
+const stylisticConfig = stylistic.configs.customize({
+  semi: true,
+  quotes: 'single',
+  quoteProps: 'as-needed',
+  arrowParens: true,
+  braceStyle: '1tbs',
 });
 
 const typescriptConfig = config(
@@ -64,8 +63,8 @@ const typescriptConfig = config(
 export default config(
   { ignores: ['dist', 'coverage'] },
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
-  pluginJs.configs.recommended,
-  stylistic.configs['recommended-flat'],
+  js.configs.recommended,
+  stylisticConfig,
   ...typescriptConfig,
   { rules: { ...eslintRules, ...stylisticRules, ...typescriptRules } },
 );
