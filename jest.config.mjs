1d5f5e4750dd2037c87f1075701c08bd3395847e
Manuel Fernández <manferlo81@gmail.com>
2024-11-09 10:19:48 +0700

    chore: Skip coverage on `SKIP_COVERAGE` env var

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,15 +3,14 @@ const config = {
   cacheDirectory: 'node_modules/.cache/jest',
   preset: 'ts-jest',
 
-  collectCoverage: true,
+  collectCoverage: !process.env.SKIP_COVERAGE,
   collectCoverageFrom: [
     'src/**',
   ],
   coverageDirectory: 'coverage',
-  coverageReporters: [
-    process.env.CI ? 'json' : 'lcov',
-    'text',
-  ],
+  coverageReporters: process.env.CI
+    ? ['json', 'clover', 'cobertura']
+    : ['html', 'text'],
 
   testMatch: [
     '**/__test__/**/*.test.ts',
