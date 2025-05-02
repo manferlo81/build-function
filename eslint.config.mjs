b7192fddc91db3737304e57cd35566162a96be55
Manuel Fernández <manferlo81@gmail.com>
2025-05-02 11:01:30 +0700

    chore: Setup `eslint-plugin-import-x`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,35 @@
 import pluginJavascript from '@eslint/js';
 import pluginStylistic from '@stylistic/eslint-plugin';
+import { flatConfigs as pluginImportConfigs } from 'eslint-plugin-import-x';
 import globals from 'globals';
 import { config, configs as typescriptConfigs } from 'typescript-eslint';
 
 const javascriptPluginConfig = config(
   pluginJavascript.configs.recommended,
-  {
-    rules: normalizeRules({
-      'no-useless-rename': 'error',
-      'object-shorthand': 'error',
-      'prefer-template': 'error',
-      'no-useless-concat': 'error',
-    }),
-  },
+  normalizeRulesConfig({
+    'no-useless-rename': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+    'no-useless-concat': 'error',
+  }),
+);
+
+const importPluginConfig = config(
+  pluginImportConfigs.recommended,
+  pluginImportConfigs.typescript,
+  normalizeRulesConfig('import-x', {
+    'consistent-type-specifier-style': 'error',
+    'no-useless-path-segments': 'error',
+    'no-absolute-path': 'error',
+    'no-cycle': 'error',
+  }),
 );
 
 const stylisticPluginConfig = config(
+  // Disable rule until @stylistic/eslint-plugin types are fixed
+  // https://github.com/eslint-stylistic/eslint-stylistic/issues/762
+  //
+  // eslint-disable-next-line import-x/no-named-as-default-member
   pluginStylistic.configs.customize({
     quotes: 'single',
     indent: 2,
@@ -24,29 +38,25 @@ const stylisticPluginConfig = config(
     quoteProps: 'as-needed',
     braceStyle: '1tbs',
   }),
-  {
-    rules: normalizeRules('@stylistic', {
-      'linebreak-style': 'unix',
-      'no-extra-parens': 'all',
-      'no-extra-semi': 'error',
-      'padded-blocks': 'off',
-    }),
-  },
+  normalizeRulesConfig('@stylistic', {
+    'linebreak-style': 'unix',
+    'no-extra-parens': 'all',
+    'no-extra-semi': 'error',
+    'padded-blocks': 'off',
+  }),
 );
 
 const typescriptPluginConfig = config(
+  { languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: process.cwd() } } },
   typescriptConfigs.strictTypeChecked,
   typescriptConfigs.stylisticTypeChecked,
+  normalizeRulesConfig('@typescript-eslint', {
+    'array-type': { default: 'array-simple', readonly: 'array-simple' },
+    'no-explicit-any': 'off',
+  }),
   {
-    languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: process.cwd() } },
-    rules: normalizeRules('@typescript-eslint', {
-      'array-type': { default: 'array-simple', readonly: 'array-simple' },
-      'no-explicit-any': 'off',
-    }),
-  },
-  {
-    files: ['**/*.{js,mjs,cjs}'],
     ...typescriptConfigs.disableTypeChecked,
+    files: ['**/*.{js,mjs,cjs}'],
   },
 );
 
@@ -55,32 +65,37 @@ export default config(
   { languageOptions: { globals: { ...globals.node, ...globals.browser } } },
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   javascriptPluginConfig,
+  importPluginConfig,
   stylisticPluginConfig,
   typescriptPluginConfig,
 );
 
-function normalizeRules(pluginName, rules) {
-  if (!rules && pluginName) return normalizeRules(null, pluginName);
+function normalizeRulesConfig(pluginName, rules) {
+  if (!rules && pluginName) return normalizeRulesConfig(null, pluginName);
+  const entries = Object.entries(rules);
+  if (!entries.length) return {};
   const normalizeEntry = createEntryNormalizer(pluginName);
-  return Object.fromEntries(Object.entries(rules).map(normalizeEntry));
+  const entriesNormalized = entries.map(normalizeEntry);
+  const rulesNormalized = Object.fromEntries(entriesNormalized);
+  return { rules: rulesNormalized };
 }
 
 function createEntryNormalizer(pluginName) {
   if (!pluginName) return ([ruleName, ruleEntry]) => [ruleName, normalizeRuleEntry(ruleEntry)];
-  const normalizeRuleName = createPluginRuleNameNormalizer(pluginName);
+  const normalizeRuleName = createPluginKeyNormalizer(pluginName);
   return ([ruleName, ruleEntry]) => [normalizeRuleName(ruleName), normalizeRuleEntry(ruleEntry)];
 }
 
-function createPluginRuleNameNormalizer(pluginName) {
+function createPluginKeyNormalizer(pluginName) {
   const pluginPrefix = `${pluginName}/`;
-  return (ruleName) => {
-    if (ruleName.startsWith(pluginPrefix)) return ruleName;
-    return `${pluginPrefix}${ruleName}`;
+  return (key) => {
+    if (key.startsWith(pluginPrefix)) return key;
+    return `${pluginPrefix}${key}`;
   };
 }
 
 function normalizeRuleEntry(entry) {
   if (Array.isArray(entry)) return entry;
-  if (['off', 'warn', 'error'].includes(entry)) return entry;
+  if (['error', 'off', 'warn'].includes(entry)) return entry;
   return ['error', entry];
 }
--- src/hash.ts
+++ src/hash.ts
@@ -6,7 +6,9 @@ let hash: ((object: object, ...others: any[]) => string) | undefined;
 if (objectHash) {
 
   hash = (object: object, ...others: unknown[]): string => others.reduce<string>(
+    // eslint-disable-next-line import-x/no-named-as-default-member
     (main, other) => `${main}${objectHash.MD5(other as never)}`,
+    // eslint-disable-next-line import-x/no-named-as-default-member
     objectHash.sha1(object),
   );
 
